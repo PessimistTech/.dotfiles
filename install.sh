50bf0fef2ed54409162dc32de1bfdb9a839e444e
jwilson <jadenwilson717@gmail.com>
2022-03-30 22:28:13 -0700

    dir

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then 
-	ln -sf gitlab/jwilson/dotfiles/.vimrc ~/.vimrc
-	ln -sf gitlab/jwilson/dotfiles/.tmux.conf ~/.tmux.conf
+	ln -sf gitlab/jwilson/dotfiles/dotfiles/.vimrc ~/.vimrc
+	ln -sf gitlab/jwilson/dotfiles/dotfiles/.tmux.conf ~/.tmux.conf
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then 
-	ln -rsf .vimrc ~/.vimrc
-	ln -rsf .tmux.conf ~/.tmux.conf
+	ln -rsf dotfiles/.vimrc ~/.vimrc
+	ln -rsf dotfiles/.tmux.conf ~/.tmux.conf
 fi
